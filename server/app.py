b2eab204807678559f9b2d116226f84a4886ce3f
scarlet <sarahscarlet641@gmail.com>
2024-07-14 20:40:42 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -5,11 +5,13 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 CORS(app)
+
 migrate = Migrate(app, db)
 
 db.init_app(app)
